f84949576470176dd5777d0b72039a198cd08468
Ajithramgopal <ajithram.gopal@gmail.com>
2025-09-23 14:43:30 +0530

    add button in dashboard

--- apartment/src/DashBoard.js
+++ apartment/src/DashBoard.js
@@ -1,7 +1,12 @@
 export default function DashBoard() {
   return (
     <>
+      <button>Create</button>
       <h1>DashBoard</h1>
+      <button>Create</button>
+      <button>Create</button>
+      <button>Create</button>
+      <button>Create</button>
     </>
   );
 }
